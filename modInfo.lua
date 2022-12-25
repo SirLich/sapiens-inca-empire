16ef0d4afe8d3bea5d804b006ea3fde4aac7cd69
SirLich <lrkoehler77@gmail.com>
2022-12-25 22:40:22 +0100

    Initial Content Push

---
+++ modInfo.lua
@@ -0,0 +1,10 @@
+local modInfo = {
+    name = "Incan Empire",
+    description = "A civilization expansion mod for Sapiens.",
+    version = "0.0.1",
+    type = "world",
+    developer = "SirLich",
+    loadOrder = 1
+}
+
+return modInfo
